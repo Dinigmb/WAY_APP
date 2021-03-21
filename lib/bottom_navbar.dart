e8610c65414300a813bcf34ed376c8adff7f9ee1
Dini <loidolt.ch@gmail.com>
2021-03-21 21:15:57 +0100

    Navbar

---
+++ lib/bottom_navbar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class MyBottomnavbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 75,
+        padding: EdgeInsets.only(top: 5, bottom: 30),
+        color: Colors.lightBlue,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.house,
+                size: 44.0,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/');
+              },
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.waves,
+                size: 44.0,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/demo');
+              },
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.person,
+                size: 44.0,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/login');
+              },
+            ),
+          ],
+        ));
+  }
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flow_app/bottom_navbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -12,59 +13,62 @@ class HomePage extends HookWidget {
     return MaterialApp(
       title: 'Flow',
       home: Scaffold(
-          backgroundColor: Colors.white,
-          appBar: AppBar(
-            title: Center(
-              child: Row(
-                children: [
-                  Text('Flow'),
-                  Icon(
-                    Icons.house,
-                    color: Colors.white,
-                  )
-                ],
-              ),
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Flow'),
+              ],
             ),
-            backgroundColor: Colors.lightBlue,
           ),
-          body: LayoutBuilder(
-            builder: (context, constraints) {
-              return SingleChildScrollView(
-                  child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20.0,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Center(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text('Create a new Flow'),
+          backgroundColor: Colors.lightBlue,
+        ),
+        body: LayoutBuilder(
+          builder: (context, constraints) {
+            return SingleChildScrollView(
+                child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20.0,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Create a new Flow',
+                        style:
+                            TextStyle(fontSize: 30.0, color: Colors.lightBlue),
                       ),
                     ),
-                    SizedBox(height: 200.0),
-                    Asanastyps('Standing'),
-                    SizedBox(height: 200.0),
-                    Asanastyps('Backward Bending'),
-                    SizedBox(height: 200.0),
-                    Asanastyps('Forward Bending'),
-                    SizedBox(height: 200.0),
-                    Asanastyps('Spinal Twisting'),
-                    SizedBox(height: 200.0),
-                    Asanastyps('Inverted'),
-                    SizedBox(height: 200.0),
-                    Asanastyps('Core Strength'),
-                    SizedBox(height: 200.0),
-                    Asanastyps('Hip opening'),
-                    SizedBox(height: 200.0),
-                    Asanastyps('Relaxation'),
-                    SizedBox(height: 200.0),
-                  ],
-                ),
-              ));
-            },
-          )),
+                  ),
+                  SizedBox(height: 200.0),
+                  Asanastyps('Standing'),
+                  SizedBox(height: 200.0),
+                  Asanastyps('Backward Bending'),
+                  SizedBox(height: 200.0),
+                  Asanastyps('Forward Bending'),
+                  SizedBox(height: 200.0),
+                  Asanastyps('Spinal Twisting'),
+                  SizedBox(height: 200.0),
+                  Asanastyps('Inverted'),
+                  SizedBox(height: 200.0),
+                  Asanastyps('Core Strength'),
+                  SizedBox(height: 200.0),
+                  Asanastyps('Hip opening'),
+                  SizedBox(height: 200.0),
+                  Asanastyps('Relaxation'),
+                  SizedBox(height: 200.0),
+                ],
+              ),
+            ));
+          },
+        ),
+        bottomNavigationBar: MyBottomnavbar(),
+      ),
     );
   }
 }
@@ -77,11 +81,17 @@ class Asanastyps extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: DecoratedBox(
-        decoration: BoxDecoration(color: Colors.orange),
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Text(name),
+      child: SizedBox(
+        width: double.infinity,
+        child: DecoratedBox(
+          decoration: BoxDecoration(color: Colors.orange),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text(
+              name,
+              style: TextStyle(fontSize: 25.0, color: Colors.white),
+            ),
+          ),
         ),
       ),
     );
